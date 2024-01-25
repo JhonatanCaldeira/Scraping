5d49b2749be80a6d283d7da878396e671af3a749
Jhonatan Caldeira <jhonatansocaldeira@gmail.com>
2024-01-25 10:44:26 +0100

    Logging Flask Configuration

--- server.py
+++ server.py
@@ -3,10 +3,14 @@ import pymongo
 import subprocess
 import os
 import json
+import logging
+import datetime
 from simplon_scrapy.simplon_scrapy import settings
 from bson.json_util import dumps, loads 
 
 app = Flask(__name__)
+
+logging.basicConfig(filename='flask.log', encoding='utf-8', level=logging.DEBUG)
 
 @app.route("/")
 def home():
@@ -27,6 +31,13 @@ def get_random_quote():
         for document in random_document:
             return_quote.append(document["quote"])
             return_quote.append(document["author"])
+            
+            log_date_time = datetime.datetime.now(tz=datetime.timezone.utc)
+            logging.info(f'{log_date_time} | Quote: {document["quote"]} | Author: {document["author"]}')
+    except Exception as e:
+        log_date_time = datetime.datetime.now(tz=datetime.timezone.utc)
+        logging.error(f'{log_date_time} | FATAL: {str(e)}')
+        raise
     finally:    
         client.close()
 
@@ -37,8 +48,15 @@ def run_spider():
     try:
         os.chdir('simplon_scrapy')
         subprocess.run(['scrapy', 'crawl', 'quotes'], check=True)
+
+        log_date_time = datetime.datetime.now(tz=datetime.timezone.utc)
+        logging.info(f'{log_date_time} | Scrapping executed successfully')
+
         return json.dumps("Scrapping executed successfully!")
-    except subprocess.CalledProcessError:
+    except subprocess.CalledProcessError as e:
+        log_date_time = datetime.datetime.now(tz=datetime.timezone.utc)
+        logging.error(f'{log_date_time} | Erreur lors de l\'exécution du Spider | {str(e)}')
+
         return "Erreur lors de l'exécution du Spider."
 
 @app.route("/api/get_log")
